c12bf55d91e8aa8b4da13623dffdc2c5bfc2a26f
Sreenandha V R <sreenandhavr161@gmail.com>
2026-03-22 03:12:53 +0530

    axios installed

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom'
-import useAuthStore from '../store/authStore'
+import useAuthStore from '../store/store
 
 export default function ProtectedRoute({ children }) {
   const token = useAuthStore((s) => s.token)
