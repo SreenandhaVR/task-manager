b84c5b661d2116fd13efd6e0c318203108596a48
Sreenandha V R <sreenandhavr161@gmail.com>
2026-03-22 10:13:24 +0530

    feat: wire Dashboard into app router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={
-        <ProtectedRoute>
-          <div style={{ color: 'white' }}>Dashboard coming soon</div>
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   )
