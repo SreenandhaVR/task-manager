c0af78f8a5c0a5e9996a1ce98bcacd84690b57d2
Sreenandha V R <sreenandhavr161@gmail.com>
2026-03-22 10:47:57 +0530

    feat: add TaskDetail page with notes and status update

---
+++ frontend/src/pages/TaskDetail.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import TaskDetail from './pages/TaskDetail'
+import ProtectedRoute from './components/ProtectedRoute'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/tasks/:id"
+        element={
+          <ProtectedRoute>
+            <TaskDetail />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  )
+}
